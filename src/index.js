c8bcb28560df08a94c5776223dcda6c97f5f9348
hhuynh-15 <bhelhuynh13251@gmail.com>
2025-08-12 11:49:33 -0400

    Added basic js
    Added basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateText(event) {
+  event.preventDefault();
 
+  let textElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: "The generated poem will be here.",
+    autoStart: true,
+    cursor: "",
+    delay: 35,
+  });
+}
+
+let inputForm = document.querySelector("#text-input");
+inputForm.addEventListener("submit", generateText);
